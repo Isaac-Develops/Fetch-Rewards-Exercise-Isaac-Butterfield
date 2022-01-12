f08c15466bca0e4f1253660d67c4865f529c4214
Isaac Butterfield <isaacben822@gmail.com>
2022-01-12 17:17:19 -0500

    Removed unnecessary middleware and added mock database

--- index.js
+++ index.js
@@ -4,20 +4,9 @@ const port = 3000
 
 // Middleware
 app.use(express.json())
-app.use((req, res, next) => {
-    res.header({ "Access-Control-Allow-Origin": "*" })
-    res.header({
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
-    })
-    res.header({
-      "Access-Control-Allow-Headers":
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-    })
-    if (req.method == "OPTIONS") {
-      res.sendStatus(200)
-    }
-    next()
-  })
+
+// Mock database (array of objects)
+let db = []
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
