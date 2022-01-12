c9a0ae62edd257b4dc69fd0f7face257bb1b7a28
Isaac Butterfield <isaacben822@gmail.com>
2022-01-12 17:10:19 -0500

    Added middleware

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
+
+// Middleware
+app.use(express.json())
+app.use((req, res, next) => {
+    res.header({ "Access-Control-Allow-Origin": "*" })
+    res.header({
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+    })
+    res.header({
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+    })
+    if (req.method == "OPTIONS") {
+      res.sendStatus(200)
+    }
+    next()
+  })
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
