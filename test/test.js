f956a3e9a54cb81e58127f88a8c0f3d11fe68f72
Isaac Butterfield <isaacben822@gmail.com>
2022-01-13 14:24:33 -0500

    Added test descriptions

--- test/test.js
+++ test/test.js
@@ -1,2 +1,28 @@
 const assert = require('assert');
 
+describe("Transaction System Testing", () => {
+    before(() => {
+        console.log("Unit Tests for each route in system")
+    })
+})
+
+// Add Transaction Route
+describe("Add Transaction Route Test", () => {
+    before(() => {
+        console.log("Unit test for adding transactions to system")
+    })
+})
+
+// Spend Points Route
+describe("Spend Points Route Test", () => {
+    before(() => {
+        console.log("Unit test for spending points in system")
+    })
+})
+
+// Points Balance Route
+describe("Points Balance Route Test", () => {
+    before(() => {
+        console.log("Unit test for retrieving payer balances")
+    })
+})
