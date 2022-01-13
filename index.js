d6d07472395bf0aa6f9a87ac808dbedf46b3e7a1
Isaac Butterfield <isaacben822@gmail.com>
2022-01-13 14:15:37 -0500

    Added comments for add transaction route

--- index.js
+++ index.js
@@ -15,26 +15,36 @@ app.get('/', (req, res) => {
 
 // Add Transaction Route
 app.post("/add", (req, res) => {
+
+  // Check for missing parameters
   if (
     req.body.payer &&
     req.body.points &&
     req.body.timestamp
     ) {
+
+    // Check for incorrect parameter types
     if (
       isString(req.body.payer) &&
       isString(req.body.timestamp) &&
       typeof req.body.points == "number" &&
       req.body.points % 1 == 0
       ) {
+
+        // Check if timestamp is a proper date
         if (!isNaN(new Date(req.body.timestamp))) {
+
+      // Add new transaction to DB
       const newTransaction = {
         payer: req.body.payer,
         points: req.body.points,
         timestamp: new Date(req.body.timestamp),
       }
+
       db.push(newTransaction)
       res.json(newTransaction)
       res.sendStatus(200)
+      
       } else {
         res.status(400).send("Bad request. Timestamp must be a proper date.\n\n'timestamp': '2020-11-02T14:00:00Z'")
       }
