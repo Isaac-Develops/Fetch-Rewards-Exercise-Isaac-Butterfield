f2f3352bf04995655cbbf61f584c6522d01406b3
Isaac Butterfield <isaacben822@gmail.com>
2022-01-13 15:54:53 -0500

    Added status 400 tests

--- test/test.js
+++ test/test.js
@@ -15,7 +15,7 @@ describe("Add Transaction Route Test", () => {
         console.log("Unit test for adding transactions to system")
     })
 
-    it("Is returning succesful transaction addition", async () => {
+    it("Is returning status 200", async () => {
         response = await fetch(baseURL + "add", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -24,13 +24,23 @@ describe("Add Transaction Route Test", () => {
             "points": 1000,
             "timestamp": "2020-11-02T14:00:00Z"
         })
-        }).then((res) => response = res.json()).then((data) => JSON.stringify(data))
+        })
 
-        assert.equal(response, JSON.stringify({
-            "payer": "DANNON",
-            "points": 1000,
-            "timestamp": "2020-11-02T14:00:00.000Z"
-        }))
+        assert.equal(response.status, 200)
+    })
+
+    it("is returning status 400", async () => {
+        response = await fetch(baseURL + "add", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                "payer": "DANNON",
+                "points": "Wrong Type",
+                "timestamp": "2020-11-02T14:00:00Z"
+            })
+        })
+
+        assert.equal(response.status, 400)
     })
 })
 
